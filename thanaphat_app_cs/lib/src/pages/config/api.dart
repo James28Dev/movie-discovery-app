41d3735265a39f1c52d40d47af4e70196f755562
James28Dev <James28.Dev@gmail.com>
2026-03-10 23:20:35 +0700

    feat: add game module api endpoints

--- thanaphat_app_cs/lib/src/pages/config/api.dart
+++ thanaphat_app_cs/lib/src/pages/config/api.dart
@@ -4,4 +4,6 @@ class API {
   static const String VIDEO_URL = 'https://api.themoviedb.org/3/movie/';
   static const String MOVIE_API_KEY =
       '/videos?api_key=796f736bf045d2d6dcc64deb64a29f92&language=en-EN';
+  static const String GAME_API = 'http://10.73.32.111/flutterapi/api/game';
+  static const String GAME_API_IMAGE = "http://10.73.32.111/flutterapi/images/";
 }
