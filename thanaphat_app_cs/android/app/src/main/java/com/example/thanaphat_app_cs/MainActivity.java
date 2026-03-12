0ab39c9503f6fd7acd5c223799e2f27a4dbbd42d
James28Dev <James28.Dev@gmail.com>
2026-03-13 00:03:37 +0700

    fix: remove ignored files and language detection

--- thanaphat_app_cs/android/app/src/main/java/com/example/thanaphat_app_cs/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.thanaphat_app_cs;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- thanaphat_app_cs/lib/src/pages/config/api.dart
+++
@@ -1,9 +0,0 @@
-class API {
-  static const String movieUrl =
-      'https://api.themoviedb.org/3/movie/upcoming?api_key=796f736bf045d2d6dcc64deb64a29f92&language=en-EN&page=1';
-  static const String videoUrl = 'https://api.themoviedb.org/3/movie/';
-  static const String movieApiUrl =
-      '/videos?api_key=796f736bf045d2d6dcc64deb64a29f92&language=en-EN';
-  static const String gameApi = 'http://10.73.32.111/flutterapi/api/game';
-  static const String gameApiImage = "http://10.73.32.111/flutterapi/images/";
-}
