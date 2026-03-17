49beb710c02a8dbefa2c8c09115ac4c30680d3de
James28Dev <James28.Dev@gmail.com>
2026-03-17 23:18:59 +0700

    feat: integrate Game CRUD with NetworkService

--- thanaphat_app_cs/lib/src/pages/game/game_management_page.dart
+++ thanaphat_app_cs/lib/src/pages/game/game_management_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:thanaphat_app_cs/src/pages/config/app_route.dart';
+import 'package:thanaphat_app_cs/src/service/network.dart';
+
+import '../../model/game_model.dart';
 
 class GameManagementPage extends StatefulWidget {
   const GameManagementPage({super.key});
@@ -8,14 +12,39 @@ class GameManagementPage extends StatefulWidget {
 }
 
 class _GameManagementPageState extends State<GameManagementPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool? _editMode;
+  Game? _game;
+
+  // File? _image;
+  // final picker = ImagePicker();
+
+  // callback(File image) {
+  //   _image = image;
+  // }
+
+  @override
+  void initState() {
+    _editMode = false;
+    _game = Game();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Object? arguments = ModalRoute.of(context)?.settings.arguments;
+    if (arguments is Game) {
+      _game = arguments;
+      _editMode = true;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text("Game Management")),
+      appBar: _buildAppBar(),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(18),
           child: Form(
+            key: _formKey,
             child: Column(
               children: <Widget>[
                 Row(
@@ -57,47 +86,104 @@ class _GameManagementPageState extends State<GameManagementPage> {
   );
 
   TextFormField _buildIdInput() => TextFormField(
+    initialValue: _game?.gameId ?? '',
     decoration: inputStyle(label: "id"),
     keyboardType: TextInputType.number,
     onSaved: (String? value) {
-      //todo
+      _game?.gameId = value;
     },
   );
 
   TextFormField _buildNameInput() => TextFormField(
+    initialValue: _game?.gameName ?? '',
     decoration: inputStyle(label: "name"),
     onSaved: (String? value) {
-      //todo
+      _game?.gameName = value;
     },
   );
 
   TextFormField _buildDetailInput() => TextFormField(
+    initialValue: _game?.gameDetail ?? '',
     decoration: inputStyle(label: "detail"),
     onSaved: (String? value) {
-      //todo
+      _game?.gameDetail = value;
     },
   );
 
   TextFormField _buildPriceInput() => TextFormField(
+    initialValue: _game?.gamePrice.toString() ?? '',
     decoration: inputStyle(label: "price"),
     keyboardType: TextInputType.number,
     onSaved: (String? value) {
-      //todo
+      _game?.gamePrice = value.toString();
     },
   );
 
   TextFormField _buildStockInput() => TextFormField(
+    initialValue: _game?.gameStock.toString() ?? '',
     decoration: inputStyle(label: "stock"),
     keyboardType: TextInputType.number,
     onSaved: (String? value) {
-      //todo
+      _game?.gameStock = value.toString();
     },
   );
 
   TextFormField _buildImageInput() => TextFormField(
+    initialValue: _game?.gameImg ?? '',
     decoration: inputStyle(label: "image"),
     onSaved: (String? value) {
       //todo
     },
+      _game?.gameImg = value;
+    },
+  );
+
+  AppBar _buildAppBar() => AppBar(
+    centerTitle: false,
+    title: Text((_editMode ?? false) ? 'Edit Game' : 'Add Game'),
+    actions: [
+      IconButton(
+        onPressed: () async {
+          _formKey.currentState?.save();
+          FocusScope.of(context).requestFocus(FocusNode());
+          if ((_editMode ?? false)) {
+            //todo Edit
+            try {
+              final message = await NetworkService().editGameDioNoImage(_game!);
+              if (message == 'Edit Successfully') {
+                Navigator.pushNamed(context, AppRoute.gameRoute);
+              } else {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text('Edit Failed')));
+              }
+            } catch (ex) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('Edit Failed')));
+            }
+          } else {
+            //todo Add Game No Image
+            try {
+              final message = await NetworkService().addGameDioNoImage(_game!);
+              if (message == 'Add Successfully') {
+                Navigator.pushNamed(context, AppRoute.gameRoute);
+              } else {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text('Add Failed')));
+              }
+            } catch (ex) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('Add Failed')));
+            }
+          }
+        },
+        icon: Icon(Icons.save, color: Colors.black),
+      ),
+    ],
+  );
+}
   );
 }
