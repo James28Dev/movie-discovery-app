b6377f204a994287079885e1743caa9c3a563415
James28Dev <James28.Dev@gmail.com>
2026-03-17 23:23:49 +0700

    feat: update image handling in GameManagementPage

--- thanaphat_app_cs/lib/src/pages/game/game_management_page.dart
+++ thanaphat_app_cs/lib/src/pages/game/game_management_page.dart
@@ -1,6 +1,9 @@
+// import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:thanaphat_app_cs/src/pages/config/app_route.dart';
 import 'package:thanaphat_app_cs/src/service/network.dart';
+// import 'package:image_picker/image_picker.dart';
 
 import '../../model/game_model.dart';
 
@@ -66,6 +69,7 @@ class _GameManagementPageState extends State<GameManagementPage> {
                 ),
                 SizedBox(height: 12.0),
                 _buildImageInput(),
+                // ProductImage(callback, image: _game?.gameImg ?? ''),
               ],
             ),
           ),
@@ -132,8 +136,6 @@ class _GameManagementPageState extends State<GameManagementPage> {
     initialValue: _game?.gameImg ?? '',
     decoration: inputStyle(label: "image"),
     onSaved: (String? value) {
-      //todo
-    },
       _game?.gameImg = value;
     },
   );
@@ -185,5 +187,149 @@ class _GameManagementPageState extends State<GameManagementPage> {
     ],
   );
 }
+
+/*
+class ProductImage extends StatefulWidget {
+  final Function callBack;
+  final String image;
+
+  const ProductImage(this.callBack, {Key? key, required this.image})
+    : super(key: key);
+
+  @override
+  _ProductImageState createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  File? _imageFile;
+  String? _image;
+  final _picker = ImagePicker();
+
+  @override
+  void initState() {
+    _image = widget.image;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _imageFile?.delete();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[_buildPickerImage(), _buildPreviewImage()],
+      ),
+    );
+  }
+
+  dynamic _buildPreviewImage() {
+    if ((_image == null || _image!.isEmpty) && (_imageFile == null)) {
+      return SizedBox();
+    }
+
+    final container = (Widget child) => Container(
+      color: Colors.grey[100],
+      margin: EdgeInsets.only(top: 4),
+      alignment: Alignment.center,
+      height: 350,
+      child: child,
+    );
+
+    return _image != null
+        ? container(Image.network('${API.gameApiImage}/$_image'))
+        : Stack(
+            children: [
+              container(Image.file(_imageFile!)),
+              _buildDeleteImageButton(),
+            ],
+          );
+  }
+
+  OutlinedButton _buildPickerImage() => OutlinedButton.icon(
+    icon: Icon(Icons.image),
+    label: Text('image'),
+    onPressed: () {
+      _modalPickerImage();
+    },
+  );
+
+  void _modalPickerImage() {
+    final buildListTile = (IconData icon, String title, ImageSource source) =>
+        ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          onTap: () {
+            Navigator.of(context).pop();
+            _pickImage(source);
+          },
+        );
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              buildListTile(
+                Icons.photo_camera,
+                "Take a picture from camera",
+                ImageSource.camera,
+              ),
+              buildListTile(
+                Icons.photo_library,
+                "Choose from photo library",
+                ImageSource.gallery,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _pickImage(ImageSource source) {
+    _picker
+        .pickImage(
+          source: source,
+          imageQuality: 70,
+          maxHeight: 500,
+          maxWidth: 500,
+        )
+        .then((file) {
+          if (file != null) {
+            setState(() {
+              _imageFile = File(file.path);
+              _image = null;
+              widget.callBack(_imageFile);
+            });
+          }
+        })
+        .catchError((error) {
+          //todo
+        });
+  }
+
+  Positioned _buildDeleteImageButton() => Positioned(
+    right: 0,
+    child: IconButton(
+      onPressed: () {
+        print('click delete image');
+        setState(() {
+          _imageFile = null;
+          //widget.callBack(null);
+        });
+      },
+      icon: Icon(Icons.clear, color: Colors.black54),
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+    ),
   );
 }
+*/
